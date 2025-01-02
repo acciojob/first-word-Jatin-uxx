11d569e0cecce91db0d972c1e73ac0cb4a2d5bb6
Jatin-uxx <rawatjatin2001@gmail.com>
2025-01-02 12:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function firstWord(s) {
+function firstWord(str) {
   // your code here
+	
+let newstr=""
+for(let i=0;i<str.length;i++)
+{
+    if(str.charAt(i)==" ")
+    {
+        break;
+    }
+    else
+    {
+        newstr+=str.charAt(i)
+    }
+}
+return newstr 
 }
 
 // Do not change the code below
